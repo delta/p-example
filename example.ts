546d7171e3985787af7abd9fe8ae3173ae57e774
aananthv <aananthv@outlook.com>
2020-11-09 11:58:10 +0530

    feat: Add layouts, middleware

--- example.ts
+++ example.ts
@@ -2,13 +2,20 @@ import config from 'config';
 
 import GeneralController from "./controllers/general.controller";
 
+import ExampleMiddleware from "./middleware/example.middleware";
+
 const controllerClasses = [
     GeneralController
+]
+
+const middleware = [
+    ExampleMiddleware
 ]
 
 export default () => {
     // Access config by 
     return {
         controllerClasses,
+        middleware
     }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 function Home() {
     return <>
-        <h1>Hello World!!</h1>
+        <h1>Hello World</h1>
         <Link href={"/plugins/p-example/about"} as={"/example/about"}>About</Link>
         <Link href={"/plugins/p-example/dynamic?foo=home"} as={"/example/dynamic/home"}>Dynamic (Home)</Link>
     </>
 }
 
-export default Home;
+export default Object.assign(Home, { layout: "main-content-layout" });
